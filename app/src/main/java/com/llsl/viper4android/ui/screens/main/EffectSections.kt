ec5d84da86a33e3efcf2c8d09f614ca73318f336
zhaoyang <zhaoyangliu10@gmail.com>
2026-08-12 23:52:32 +0800

    fix: Set max height for effect description dialog

--- app/src/main/java/com/llsl/viper4android/ui/screens/main/EffectSections.kt
+++ app/src/main/java/com/llsl/viper4android/ui/screens/main/EffectSections.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -67,7 +68,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.hapticfeedback.HapticFeedbackType
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalHapticFeedback
+import androidx.compose.ui.platform.LocalWindowInfo
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -196,9 +199,14 @@ fun EffectSection(
             onDismissRequest = { showHelpDialog = false },
             title = { Text(text = title) },
             text = {
-                Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
-                    RichText(text = stringResource(descriptionRes))
-                }
+                val maxHeight =
+                    with(LocalDensity.current) {
+                        LocalWindowInfo.current.containerSize.height
+                            .toDp() / 2
+                    }
+                Column(
+                    modifier = Modifier.heightIn(max = maxHeight).verticalScroll(rememberScrollState()),
+                ) { RichText(text = stringResource(descriptionRes)) }
             },
             confirmButton = {
                 TextButton(onClick = { showHelpDialog = false }) {
